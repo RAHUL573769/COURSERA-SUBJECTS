5c74d4a3287bb0c5e1c1e967699974331d6dcc79
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-01-24 11:41:36 +0600

    chages made

---
+++ A_Programming Language/Java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int mySecondNumber = 13;
+        System.out.println(mySecondNumber);
+    }
+}
---
+++ A_Programming Language/Java/conditnal.java
@@ -0,0 +1,8 @@
+public class conditnal {
+    public static void main(String[] args) {
+        boolean isFalse = false;
+        if (isFalse == false) {
+            System.out.println("DonotRun");
+        }
+    }
+}
