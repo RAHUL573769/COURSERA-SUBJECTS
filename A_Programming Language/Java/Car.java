aebfc71f3b3442969f25be3f92fb1823730cb0ee
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-01-25 10:53:49 +0600

    changes made

---
+++ A_Programming Language/Java/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+
+    public String make = "India";
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertable;
+
+    public void describeCar() {
+        System.out.println(doors + "Doors");
+
+        System.out.println(make + "Doors");
+    }
+
+}
