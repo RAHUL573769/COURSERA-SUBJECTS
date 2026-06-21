0b5ef1bda841752571caad4aedf15b4f964271b3
Rahul Rudra <rahulrudra665@gmail.com>
2026-06-21 11:46:08 +0600

    new comtt

---
+++ 1A_Programming Language(Feb-Marc)/Java/Java/Section06/Main.java
@@ -0,0 +1,13 @@
+package Java.Section06;
+
+public class Main {
+    public static void main(String[] args) {
+    int value=3;
+    switch (value) {
+        case 1 -> System.out.println("Value was 1");
+        case 3 -> System.out.println("Vlaure ");
+        default -> System.out.println("Not Found");
+    }
+
+}
+}
